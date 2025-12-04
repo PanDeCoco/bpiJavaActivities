7e1562d3d472bd8d4476f4a03d32097bc7277958
Zanice <zaniceshemuel09@gmail.com>
2025-12-04 20:05:58 +0800

    Module 1 activity5

---
+++ MyClass5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class MyClass5 {
+  public static void main(String args[]) {
+    int sum = 0;
+
+    for(int x = 1; x <= 50; x++){
+      sum = sum + x;
+    }
+    System.out.println(sum);
+  }
+}
