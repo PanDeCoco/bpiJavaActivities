13236b57dfa21f9bf53b5a980a32091569ffa678
Zanice <zaniceshemuel09@gmail.com>
2025-12-04 20:31:11 +0800

    Module 2 activity1

---
+++ Main.java
@@ -0,0 +1,14 @@
+package seatwork1;
+
+public class book {
+    String title = "title of the book";
+    int page = 2;
+
+
+    void booktitle() {
+        System.out.print("The title of the book " + title);
+    }
+    void pagenum() {
+        System.out.print("Number of pages " + page);
+    }
+}
---
+++ book.java
@@ -0,0 +1,12 @@
+public class book {
+    String title = "chronicles of bogart";
+    int page = 2;
+
+
+    void booktitle() {
+        System.out.println("The title of the book " + title);
+    }
+    void pagenum() {
+        System.out.println("Number of pages " + page);
+    }
+}
