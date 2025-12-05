a30ca675f4759fd4ac9a84c3ed4ea3066ecfa205
Zanice <zaniceshemuel09@gmail.com>
2025-12-05 17:51:50 +0800

    Module 2 activity5

---
+++ Executable.java
@@ -0,0 +1,12 @@
+public class  Executable {
+   // seatwork: add your methods here
+
+   public void run() {
+	String message = "Opening MS Excel...";
+	System.out.println(message);
+	}
+   public void stop() {
+		System.out.println("Stopping MS Word...");
+	}
+
+}
---
+++ MSWord.java
@@ -0,0 +1,12 @@
+public class MSWord extends Executable {
+	
+	@Override
+	public void run() {
+		System.out.println("Opening MS Word...");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stopping MS Word...");
+	}
+}
