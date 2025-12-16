270c60a6cf4c0be312f2ca34dfc40368964383a4
zanice <87259700+PanDeCoco@users.noreply.github.com>
2025-12-16 16:08:29 +0800

    m3 Activity1

---
+++ Main.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+ 
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		LinkedList<String> products = new LinkedList<>();
+ 
+		products.add("Laptop");
+		products.add("Mouse");
+		products.add("Keyboard");
+		products.add("Monitor");
+		products.add("Printer");
+ 
+		System.out.println("All products:");
+ 
+		for (int i = 0; i < products.size();i++) {
+			System.out.println((i + 1) + "." + products.get(i));
+		}
+ 
+		products.add("Webcam");
+		products.remove("Mouse");
+		System.out.println(" ");
+		System.out.println("All products:");
+		for (int i  = 0; i < products.size();i++) {
+			System.out.println((i + 1) + "." + products.get(i));
+		}
+ 
+		System.out.println(" ");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter product name to search: ");
+		String search = sc.nextLine();
+ 
+		boolean found = false;
+ 
+		for (String product : products) {
+		    if (product.equalsIgnoreCase(search)) {
+		        System.out.println("Product found: " + product);
+		        found = true;
+		        break;
+		    }
+		}
+ 
+		if (!found) {
+		    System.out.println("Product not found.");
+		}
+ 
+ 
+	    sc.close();
+ 
+}
+}
